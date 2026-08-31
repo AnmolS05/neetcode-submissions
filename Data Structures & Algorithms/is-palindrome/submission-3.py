7c8671c3f4d18851d822e8fb14a68969c52324bc
Anmol S Poojary <anmolspoojary@gmail.com>
2026-08-31 23:19:47 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s)-1
+        while left<right:
+            while left<right and s[left].isalnum():
+                left += 1
+            while left<right and s[right].isalnum():
+                right -= 1
+            while s[left].lower() != s[right].lower():
+                return True
+            left += 1
+            right -= 1
+        return False
